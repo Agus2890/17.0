89d9ea9407163af2338117c2920b3bfeded666bf
Your Name <you@example.com>
2024-11-20 17:10:13 +0000

    limpienaod

--- pos_defined_discount/models/res_config_settings.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class discountConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
-    
-    pos_custom_discount_ids = fields.One2many(string="Discounts", comodel_name="pos.custom.discount", readonly=False, related="pos_config_id.pos_custom_discount_ids")
-    
-    
